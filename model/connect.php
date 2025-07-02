878acd686fac0d87ad7121705362760f5aa01a61
taitran2912 <trantantai.3013@gmail.com>
2025-07-02 11:59:22 +0700

    tai lieu

--- model/connect.php
+++ model/connect.php
@@ -1,7 +1,6 @@
 <?php
 class clsketnoi{
     public function moKetNoi(){
-
         $con = mysqli_connect("localhost", "root", "", "nongsan");
         mysqli_set_charset($con,'utf8');
         return $con;
